137b0e6c47c90a013831177dbe813968d869813e
Sacid <148653854+Sacid66@users.noreply.github.com>
2024-11-26 16:55:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -95,4 +95,6 @@ def analyze_hadis():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Tüm IP adreslerine açık ve port 5000 üzerinde çalışacak şekilde ayarladık
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
